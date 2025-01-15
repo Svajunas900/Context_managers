c2da10b7fec2c4d61c5ede3d9dd974d68522509c
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-15 09:12:19 +0200

    Change __exit__ method without with

--- context_manager.py
+++ context_manager.py
@@ -1,26 +1,30 @@
 class RemoveNonUnicodeCharacters:
 
   def __init__(self, original_content, new_file_path):
-      self.new_file_path = new_file_path
-      self.original_content = original_content
-      self.new_content = None
-      self.first_five_words = []
+    self.new_file_path = new_file_path
+    self.original_content = original_content
+    self.new_content = None
+    self.first_five_words = []
 
   def __enter__(self):
-      string_encode = self.original_content.encode("ascii", "ignore")
-      string_decode = string_encode.decode()
-      string_list = string_decode.split()
-      result = []
-      for word in string_list:
-        if len(word) > 70:
-          continue
-        else:
-          result.append(word)
-      self.first_five_words = result[0:5]
-      self.new_content = " ".join(result)
-      return " ".join(self.first_five_words)
+    string_encode = self.original_content.encode("ascii", "ignore")
+    string_decode = string_encode.decode()
+    string_list = string_decode.split()
+    result = []
+    for word in string_list:
+      if len(word) > 70:
+        continue
+      else:
+        result.append(word)
+    self.first_five_words = result[0:5]
+    self.new_content = " ".join(result)
+    return " ".join(self.first_five_words)
 
   def __exit__(self, exc_type, exc_val, exc_tb):
-      with open(self.new_file_path, 'w') as file:
-        file.write(self.new_content)
+    file = open(self.new_file_path, 'w')
+    try:
+      file.write(self.new_content)   
+    finally:
+      file.close()
+      
       
